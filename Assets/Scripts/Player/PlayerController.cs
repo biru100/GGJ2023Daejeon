70a016e59bb18252bf018cccf69b1cacaf00306c
DESKTOP-5KSGJN4\gywkd <gywkd10@naver.com>
2023-02-03 22:48:06 +0900

    대충 플레이어 만듬 쿠쿨룩삥봉

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -2,17 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[System.Serializable]
+public enum PlayerState
+{
+    INSAM,
+    ZOMBIE
+}
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField]
+    private PlayerState playerState;
+    [SerializeField]
+    private float speed;
+
+    private Rigidbody2D rigid;
+    private string playerHorizontal;
+    private string playerVertical;
+    private void Awake()
     {
-        
+        rigid = GetComponent<Rigidbody2D>();
+        playerHorizontal = playerState == PlayerState.INSAM ? "InsamHorizontal" : "ZombieHorizontal";
+        playerVertical = playerState == PlayerState.INSAM ? "InsamVertical" : "ZombieVertical";
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    void Move()
+    {
+        Vector2 dir = new Vector2(Input.GetAxisRaw(playerHorizontal), Input.GetAxisRaw(playerVertical)).normalized;
+
+        rigid.velocity = dir * speed * Time.deltaTime;
     }
 }
